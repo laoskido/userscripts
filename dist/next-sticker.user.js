1cd718f5dba686ccf8f04c3a4d104ee5f2db385d
laoskido <142037427+laoskido@users.noreply.github.com>
2024-07-26 17:46:31 +0700

    Update next-sticker.user.js

--- dist/next-sticker.user.js
+++ dist/next-sticker.user.js
@@ -7,7 +7,7 @@
 // @supportURL  https://github.com/phonglk/userscripts/pulls
 // @match       https://*.voz.vn/*
 // @namespace   Next
-// @updateURL   https://raw.githubusercontent.com/phonglk/userscripts/master/dist/next-sticker.user.js
+// @updateURL   https://github.com/laoskido/userscripts/blob/master/dist/next-sticker.user.js
 // @connect     imgur.com
 // @source      https://github.com/laoskido/userscripts/tree/master/src/next/sticker
 // @grant       GM_getValue
